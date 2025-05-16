0a0343aba4744e956c74444ac9a9f2c93b246935
kruti <kruti.dimpi@gmail.com>
2025-05-16 23:29:40 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,7 @@
 // import logo from './logo.svg';
 import "./App.css";
-import Login from "./components/login/Login";
-import SignIn from "./components/login/SignIn";
+
 import Routing from "./components/Routing/Routing";
-import Calculator from "./components/ToolOperations/Calculator";
-import Formulas from "./components/ToolOperations/Formulas";
-import GraphsAndGeomatry from "./components/ToolOperations/GraphsAndGeomatry";
-import Quizes from "./components/ToolOperations/Quizes";
 
 function App() {
   return (
